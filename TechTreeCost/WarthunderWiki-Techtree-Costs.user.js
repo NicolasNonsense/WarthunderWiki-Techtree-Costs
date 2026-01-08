c552b42c4d8a77a7143258c4c1c160070e342a9d
NicolasNonsense <133248750+NicolasNonsense@users.noreply.github.com>
2026-01-08 22:31:38 +0100

    Add update and download URLs to user script

--- TechTreeCost/WarthunderWiki-Techtree-Costs.user.js
+++ TechTreeCost/WarthunderWiki-Techtree-Costs.user.js
@@ -3,6 +3,8 @@
 // @namespace    https://wiki.warthunder.com/
 // @version      1.0.0
 // @description  Replaces BR with RP and SL cost; Sums up Cost per Rank and Total.
+// @updateURL    https://raw.githubusercontent.com/NicolasNonsense/WarthunderWiki-Techtree-Costs/main/TechTreeCost/WarthunderWiki-Techtree-Costs.user.js
+// @downloadURL  https://raw.githubusercontent.com/NicolasNonsense/WarthunderWiki-Techtree-Costs/main/TechTreeCost/WarthunderWiki-Techtree-Costs.user.js
 // @match        https://wiki.warthunder.com/ground*
 // @match        https://wiki.warthunder.com/aviation*
 // @match        https://wiki.warthunder.com/helicopters*
